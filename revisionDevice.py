d50fe0f24ed9fe3035e9bd2c3b75784f2a597977
puigalex <puig.alejandro24@gmail.com>
2017-07-05 20:24:56 -0500

    Comentario

--- revisionDevice.py
+++ revisionDevice.py
@@ -1,3 +1,5 @@
+#Una simple prueba para evr si la version de python esta corriendo con GPU 
+
 import tensorflow as tf
 with tf.device('/gpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
